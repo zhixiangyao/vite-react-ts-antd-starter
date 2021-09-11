5de8f246d52f72878af2c0a105cebb0746d29f17
zhixiangyao <blueskyzx@icloud.com>
2021-09-11 18:20:13 +0800

    feat

--- src/components/Line.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Line = () => <div className="w-full h-1px bg-gray-200 mt-2 mb-8" />
-
-export default Line
--- src/views/RegistrantList/components/RegistrantForm/index.tsx
+++ src/views/RegistrantList/components/RegistrantForm/index.tsx
@@ -4,7 +4,6 @@ import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
 import { Modal, Button, Row, Col } from 'antd'
 import moment from 'moment'
 
-import Line from '/@/components/Line'
 import UploadAvatar from '/@/components/UploadAvatar'
 import {
   addLookEdit,
@@ -38,227 +37,125 @@ const RegistrantForm: React.FC<Props> = (props) => {
   }
 
   const BasicInfoView = (
-    <Row gutter={24}>
-      <Col span={8} sm={10}>
-        <Form.Item name="id" rules={[{ required: true, message: '请输入你的编号!' }]} label="编号">
-          <Input disabled={disabled} />
-        </Form.Item>
-
-        <Form.Item
-          name="registrationTime"
-          rules={[{ required: true, message: '请选择你的填表时间!' }]}
-          label="填表时间"
-        >
-          <DatePicker disabled={disabled} />
-        </Form.Item>
-
-        <Form.Item
-          name="name"
-          rules={[{ required: true, message: '请输入你的姓名!' }]}
-          label="姓名"
-        >
-          <Input disabled={disabled} />
-        </Form.Item>
-
-        <Form.Item
-          name="birthDate"
-          rules={[{ required: true, message: '请选择你的出生年月!' }]}
-          label="出生年月"
-        >
-          <DatePicker picker="month" disabled={disabled} />
-        </Form.Item>
-
-        <Form.Item name="sex" rules={[{ required: true, message: '请选择你的性别!' }]} label="性别">
-          <Radio.Group disabled={disabled}>
-            <Radio value={0}>男</Radio>
-            <Radio value={1}>女</Radio>
-          </Radio.Group>
-        </Form.Item>
-      </Col>
-
-      <Col span={8} sm={10}>
-        <Form.Item
-          name="maritalStatus"
-          rules={[{ required: true, message: '请选择你的婚姻!' }]}
-          label="婚姻"
-        >
-          <Select disabled={disabled}>
-            {maritalStatusList.map(({ lable, value }) => (
-              <Select.Option key={value} value={value}>
-                {lable}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-
-        <Form.Item
-          name="censusEegister"
-          rules={[{ required: true, message: '请输入你的户籍!' }]}
-          label="户籍"
-        >
-          <Input disabled={disabled} />
-        </Form.Item>
-
-        <Form.Item
-          name="category"
-          rules={[{ required: true, message: '请选择你的户口性质!' }]}
-          label="户口性质"
-        >
-          <Select disabled={disabled}>
-            <Select.Option value={0}>城镇</Select.Option>
-            <Select.Option value={1}>农村</Select.Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item
-          name="nation"
-          rules={[{ required: true, message: '请输入你的民族!' }]}
-          label="民族"
-        >
-          <Input disabled={disabled} />
-        </Form.Item>
-
-        <Form.Item
-          name="politicalIdentity"
-          rules={[{ required: true, message: '请选择你的党/团!' }]}
-          label="党/团"
-        >
-          <Select disabled={disabled}>
-            {politicalIdentityList.map(({ lable, value }) => (
-              <Select.Option key={value} value={value}>
-                {lable}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-      </Col>
-
-      <Col span={8} sm={4}>
-        <UploadAvatar disabled={disabled} />
-      </Col>
-    </Row>
-  )
-
-  const ContactDetailsInfoView = (
-    <Row gutter={24}>
-      <Col span={8}>
-        <Form.Item name="wechatID" label="微信号">
-          <Input disabled={disabled} />
-        </Form.Item>
-        <Form.Item name="email" label="E-mail">
-          <Input disabled={disabled} />
-        </Form.Item>
-        <Form.Item name="emergencyContact" label="紧急联系人">
-          <Input disabled={disabled} />
-        </Form.Item>
-      </Col>
-
-      <Col span={8}>
-        <Form.Item name="qqID" label="QQ号">
-          <Input disabled={disabled} />
-        </Form.Item>
-        <Form.Item name="emergencyContactRelationship" label="关系">
-          <Input disabled={disabled} />
-        </Form.Item>
-      </Col>
-
-      <Col span={8}>
-        <Form.Item name="phone" label="手机号">
-          <Input disabled={disabled} />
-        </Form.Item>
-        <Form.Item name="emergencyContactPhone" label="联系电话">
-          <Input disabled={disabled} />
-        </Form.Item>
-      </Col>
-    </Row>
-  )
-
-  const WorkExperienceListInfoView = (
-    <Form.List name="workList">
-      {(fields, { add, remove }) => (
-        <>
-          {fields.map(({ key, name, fieldKey, ...restField }) => (
-            <Space size={100} key={key} style={{ marginBottom: 8 }} align="baseline">
-              <Form.Item
-                {...restField}
-                name={[name, 'date']}
-                fieldKey={[fieldKey, 'date']}
-                rules={[{ required: true, message: '请输入起止日期' }]}
-              >
-                <Input disabled={disabled} placeholder="起止日期" />
-              </Form.Item>
-
-              <Form.Item
-                {...restField}
-                name={[name, 'name']}
-                fieldKey={[fieldKey, 'name']}
-                rules={[{ required: true, message: '请输入工作单位' }]}
-              >
-                <Input disabled={disabled} placeholder="工作单位" />
-              </Form.Item>
-
-              <Form.Item
-                {...restField}
-                name={[name, 'rule']}
-                fieldKey={[fieldKey, 'rule']}
-                rules={[{ required: true, message: '请输入工作职位' }]}
-              >
-                <Input disabled={disabled} placeholder="工作职位" />
-              </Form.Item>
-
-              <Form.Item
-                {...restField}
-                name={[name, 'info']}
-                fieldKey={[fieldKey, 'info']}
-                rules={[{ required: true, message: '请输入离职原因' }]}
-              >
-                <Input disabled={disabled} placeholder="离职原因" />
-              </Form.Item>
-
-              <MinusCircleOutlined onClick={() => remove(name)} />
-            </Space>
-          ))}
-
-          <Form.Item>
-            <Button
-              disabled={disabled}
-              type="dashed"
-              onClick={() => add()}
-              block
-              icon={<PlusOutlined />}
-            >
-              新增工作经历
-            </Button>
-          </Form.Item>
-        </>
-      )}
-    </Form.List>
-  )
-
-  const SubmitView = (
-    <Form.Item wrapperCol={{ span: 24, offset: 12 }}>
-      <Button type="primary" size="large" htmlType="submit">
-        {addLookEdit[state]}
-      </Button>
-    </Form.Item>
-  )
-
-  const FormView = (
-    <Form
-      layout="horizontal"
-      size="small"
-      form={form}
-      labelCol={{ span: 4 }}
-      wrapperCol={{ span: 14 }}
-      initialValues={{ size: 'small' }}
-      onFinish={onFinish}
-    >
-      {BasicInfoView}
-
-      <Line />
+    <fieldset>
+      <legend>基础信息</legend>
 
       <Row gutter={24}>
-        <Col span={12}>
+        <Col span={8} sm={10}>
+          <Form.Item
+            name="id"
+            rules={[{ required: true, message: '请输入你的编号!' }]}
+            label="编号"
+          >
+            <Input disabled={disabled} />
+          </Form.Item>
+
+          <Form.Item
+            name="registrationTime"
+            rules={[{ required: true, message: '请选择你的填表时间!' }]}
+            label="填表时间"
+          >
+            <DatePicker disabled={disabled} />
+          </Form.Item>
+
+          <Form.Item
+            name="name"
+            rules={[{ required: true, message: '请输入你的姓名!' }]}
+            label="姓名"
+          >
+            <Input disabled={disabled} />
+          </Form.Item>
+
+          <Form.Item
+            name="birthDate"
+            rules={[{ required: true, message: '请选择你的出生年月!' }]}
+            label="出生年月"
+          >
+            <DatePicker picker="month" disabled={disabled} />
+          </Form.Item>
+
+          <Form.Item
+            name="sex"
+            rules={[{ required: true, message: '请选择你的性别!' }]}
+            label="性别"
+          >
+            <Radio.Group disabled={disabled}>
+              <Radio value={0}>男</Radio>
+              <Radio value={1}>女</Radio>
+            </Radio.Group>
+          </Form.Item>
+        </Col>
+
+        <Col span={8} sm={10}>
+          <Form.Item
+            name="maritalStatus"
+            rules={[{ required: true, message: '请选择你的婚姻!' }]}
+            label="婚姻"
+          >
+            <Select disabled={disabled}>
+              {maritalStatusList.map(({ lable, value }) => (
+                <Select.Option key={value} value={value}>
+                  {lable}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            name="censusEegister"
+            rules={[{ required: true, message: '请输入你的户籍!' }]}
+            label="户籍"
+          >
+            <Input disabled={disabled} />
+          </Form.Item>
+
+          <Form.Item
+            name="category"
+            rules={[{ required: true, message: '请选择你的户口性质!' }]}
+            label="户口性质"
+          >
+            <Select disabled={disabled}>
+              <Select.Option value={0}>城镇</Select.Option>
+              <Select.Option value={1}>农村</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            name="nation"
+            rules={[{ required: true, message: '请输入你的民族!' }]}
+            label="民族"
+          >
+            <Input disabled={disabled} />
+          </Form.Item>
+
+          <Form.Item
+            name="politicalIdentity"
+            rules={[{ required: true, message: '请选择你的党/团!' }]}
+            label="党/团"
+          >
+            <Select disabled={disabled}>
+              {politicalIdentityList.map(({ lable, value }) => (
+                <Select.Option key={value} value={value}>
+                  {lable}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+
+        <Col span={8} sm={4}>
+          <UploadAvatar disabled={disabled} />
+        </Col>
+      </Row>
+    </fieldset>
+  )
+
+  const OtherInfoView = (
+    <fieldset>
+      <legend>其它信息</legend>
+
+      <Row gutter={24}>
+        <Col span={8} sm={10}>
           <Form.Item name="highestEducation" label="最高学历">
             <Select disabled={disabled}>
               {highestEducationList.map(({ lable, value }) => (
@@ -282,7 +179,7 @@ const RegistrantForm: React.FC<Props> = (props) => {
           </Form.Item>
         </Col>
 
-        <Col span={12}>
+        <Col span={8} sm={10}>
           <Form.Item name="position" label="岗/职位">
             <Input disabled={disabled} />
           </Form.Item>
@@ -291,7 +188,7 @@ const RegistrantForm: React.FC<Props> = (props) => {
             <Input disabled={disabled} />
           </Form.Item>
 
-          <Form.Item name="accountAddress" label="户口所在地址">
+          <Form.Item name="accountAddress" label="户口所在地">
             <Input disabled={disabled} />
           </Form.Item>
 
@@ -300,30 +197,174 @@ const RegistrantForm: React.FC<Props> = (props) => {
           </Form.Item>
         </Col>
       </Row>
+    </fieldset>
+  )
 
-      <Line />
+  const ContactDetailsInfoView = (
+    <fieldset>
+      <legend>联系方式</legend>
+
+      <Row gutter={24}>
+        <Col span={8} sm={10}>
+          <Form.Item name="wechatID" label="微信号">
+            <Input disabled={disabled} />
+          </Form.Item>
+          <Form.Item name="email" label="E-mail">
+            <Input disabled={disabled} />
+          </Form.Item>
+          <Form.Item name="emergencyContact" label="紧急联系人">
+            <Input disabled={disabled} />
+          </Form.Item>
+        </Col>
+
+        <Col span={8} sm={10}>
+          <Form.Item name="qqID" label="QQ号">
+            <Input disabled={disabled} />
+          </Form.Item>
+          <Form.Item name="emergencyContactRelationship" label="关系">
+            <Input disabled={disabled} />
+          </Form.Item>
+        </Col>
+
+        <Col span={8} sm={10}>
+          <Form.Item name="phone" label="手机号">
+            <Input disabled={disabled} />
+          </Form.Item>
+          <Form.Item name="emergencyContactPhone" label="联系电话">
+            <Input disabled={disabled} />
+          </Form.Item>
+        </Col>
+      </Row>
+    </fieldset>
+  )
+
+  const BankInfoView = (
+    <fieldset>
+      <legend>银行信息</legend>
+
+      <Row gutter={24}>
+        <Col span={8} sm={10}>
+          <Form.Item name="providentFundAccount" label="公积金账号">
+            <Input disabled={disabled} />
+          </Form.Item>
+
+          <Form.Item name="cardNo" label="银行卡号">
+            <Input disabled={disabled} />
+          </Form.Item>
+        </Col>
+      </Row>
+    </fieldset>
+  )
+
+  const WorkExperienceListInfoView = (
+    <fieldset>
+      <legend>工作经历</legend>
+
+      <Form.List name="workList">
+        {(fields, { add, remove }) => (
+          <>
+            {fields.map(({ key, name, fieldKey, ...restField }) => (
+              <Space size={100} key={key} style={{ marginBottom: 8 }} align="baseline">
+                <Form.Item
+                  {...restField}
+                  name={[name, 'date']}
+                  fieldKey={[fieldKey, 'date']}
+                  rules={[{ required: true, message: '请输入起止日期' }]}
+                >
+                  <Input disabled={disabled} placeholder="起止日期" />
+                </Form.Item>
+
+                <Form.Item
+                  {...restField}
+                  name={[name, 'name']}
+                  fieldKey={[fieldKey, 'name']}
+                  rules={[{ required: true, message: '请输入工作单位' }]}
+                >
+                  <Input disabled={disabled} placeholder="工作单位" />
+                </Form.Item>
+
+                <Form.Item
+                  {...restField}
+                  name={[name, 'rule']}
+                  fieldKey={[fieldKey, 'rule']}
+                  rules={[{ required: true, message: '请输入工作职位' }]}
+                >
+                  <Input disabled={disabled} placeholder="工作职位" />
+                </Form.Item>
+
+                <Form.Item
+                  {...restField}
+                  name={[name, 'info']}
+                  fieldKey={[fieldKey, 'info']}
+                  rules={[{ required: true, message: '请输入离职原因' }]}
+                >
+                  <Input disabled={disabled} placeholder="离职原因" />
+                </Form.Item>
+
+                <MinusCircleOutlined onClick={() => remove(name)} />
+              </Space>
+            ))}
+
+            <Form.Item>
+              <Button
+                disabled={disabled}
+                type="dashed"
+                onClick={() => add()}
+                block
+                icon={<PlusOutlined />}
+              >
+                新增工作经历
+              </Button>
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+    </fieldset>
+  )
+
+  const RemarksInfoView = (
+    <fieldset>
+      <legend>其它</legend>
+
+      <Row gutter={24}>
+        <Col span={8} sm={10}>
+          <Form.Item name="remarks" label="备注">
+            <Input disabled={disabled} />
+          </Form.Item>
+        </Col>
+      </Row>
+    </fieldset>
+  )
+
+  const SubmitView = (
+    <Form.Item wrapperCol={{ span: 24, offset: 12 }}>
+      <Button type="primary" size="large" htmlType="submit">
+        {addLookEdit[state]}
+      </Button>
+    </Form.Item>
+  )
+
+  const FormView = (
+    <Form
+      layout="horizontal"
+      size="small"
+      form={form}
+      labelCol={{ span: 4 }}
+      wrapperCol={{ span: 14 }}
+      initialValues={{ size: 'small' }}
+      onFinish={onFinish}
+    >
+      {BasicInfoView}
+
+      {OtherInfoView}
 
       {ContactDetailsInfoView}
 
-      <Line />
-
-      <Form.Item name="providentFundAccount" label="公积金账号">
-        <Input disabled={disabled} />
-      </Form.Item>
-
-      <Form.Item name="cardNo" label="银行卡号(兴业银行)">
-        <Input disabled={disabled} />
-      </Form.Item>
-
-      <Line />
+      {BankInfoView}
 
       {WorkExperienceListInfoView}
 
-      <Line />
-
-      <Form.Item name="remarks" label="备注">
-        <Input disabled={disabled} />
-      </Form.Item>
+      {RemarksInfoView}
 
       {disabled === false && SubmitView}
     </Form>
