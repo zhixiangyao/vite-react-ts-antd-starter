2fced824ffa78bb325f88379110020b4c7e0a2eb
zhixiangyao <blueskyzx@icloud.com>
2021-10-30 15:29:01 +0800

    style

--- src/layout/components/Main.tsx
+++ src/layout/components/Main.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Main: React.FC = ({ children }) => {
   return (
     <main
-      className={`flex text-black items-center justify-start flex-col h-full pt-20 sm:pr-4 sm:pb-4 sm:pl-4 overflow-auto`}
+      className={`flex text-black items-center justify-start flex-col min-h-full pt-20 sm:pr-4 sm:pb-4 sm:pl-4 overflow-auto`}
     >
       {children}
     </main>
