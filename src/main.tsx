06c8b95d2797173748822830f576b63617fccd18
zhixiangyao <blueskyzx@icloud.com>
2025-10-18 11:47:52 +0800

    feat: add @ant-design/v5-patch-for-react-19 dependency and import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from '~/App'
 
+import '@ant-design/v5-patch-for-react-19'
+
 const rootElement = document.querySelector('#root')!
 
 const app = createRoot(rootElement!)
